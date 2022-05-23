c49b694a6a14d64656c1125e7f1da7656824421f
Peijin He <49417569+PeijinGit@users.noreply.github.com>
2022-05-23 13:29:00 +1200

    Update tests

--- UnitTests/ProcessFile_Test.cs
+++ UnitTests/ProcessFile_Test.cs
@@ -1,23 +1,30 @@
-﻿using Xunit;
+﻿using Abstraction;
+using BusinessLayer;
+using Domain;
+using Moq;
+using System.ComponentModel;
+using Xunit;
 
 namespace UnitTests
 {
     public class ProcessFile_Test
     {
         [Fact]
-        public void WhatEverTest() 
+        public void ProcessFile_Should_Execute_WriteCSV_IfTest()
         {
-            string test = "";
-            string test2 = null;
+            var csvHandler = new Mock<ICsvHandler>();
+            var selectFile = "testPath";
+            var destination = "testPath";
+            BackgroundWorker worker = new();
 
-            if (DateTime.TryParse(test, out DateTime dateTime))
-            {
-                int a = 0;
-            }
-            else 
-            {
-                int a = 0;
-            }
+            var residentsToBeConvert = new List<ResidentToBeConvert>();
+            csvHandler.Setup(x => x.ParseCsv(It.IsAny<string>())).Returns(residentsToBeConvert);
+            csvHandler.Setup(x => x.WriteCSV(It.IsAny<string>(), It.IsAny<List<ResidentConverted>>()));
+
+            var subject = new FileProcessor(csvHandler.Object);
+            subject.ProcessFile(selectFile, destination, worker);
+
+            csvHandler.Verify(x => x.WriteCSV(It.IsAny<string>(), It.IsAny<List<ResidentConverted>>()),Times.Once);
         }
     }
 }
